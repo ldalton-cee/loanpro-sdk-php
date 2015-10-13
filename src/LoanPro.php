6c1aabcc9bab7a8b1142a3d7e75b673673a11197
tofurama3000 <tolmlanmatt@gmail.com>
2015-10-13 11:06:18 -0600

    Added comments to select functions, and made logging optional. Also made it so the sdk didn't crash if logging failed. Instead, logging errors are outputed to the browser console. This is since logging may be complicated to setup properly depending on permission settings for a computer. Also, some individuals may not want logging enabled (small storage space). Logging is still enabled by default. Created public function to change logging settings. Is backwards compatible with previous versions.

--- src/LoanPro.php
+++ src/LoanPro.php
@@ -10,6 +10,12 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use ODataQuery\ODataResourcePath;
 
+/**
+ * Class LoanPro
+ * @package Simnang\LoanPro
+ *
+ * This class is the basis for all communications with the LoanPro API
+ */
 class LoanPro {
     private $endpointBase = "https://loanpro.simnang.com/api/public/api/1/";
     private $apiKey;
@@ -17,9 +23,8 @@ class LoanPro {
 
     private $log = null;
 
-    public function __construct($path = '', $level = 100) {
-        $this->log = new Logger('loanpro-sdk');
-        $this->log->pushHandler(new StreamHandler($path ?: 'loanpro-sdk.log', $level));
+    public function __construct() {
+        $this->setLoggingOptions();
     }
 
     public function getEndpointBase() {
@@ -57,14 +62,42 @@ class LoanPro {
         $this->setEndpointBase($options['endpointBase'] ? $options['endpointBase'] : $this->getEndpointBase());
     }
 
+    public function setLoggingOptions($path = '', $level = 100, $loggingEnabled = true)
+    {
+        if($loggingEnabled) {
+            $this->log = new Logger('loanpro-sdk');
+            $this->log->pushHandler(new StreamHandler($path ?: 'loanpro-sdk.log', $level));
+        }
+        else{
+            $this->disableLogging();
+        }
+    }
+
+    public function disableLogging()
+    {
+        $this->log = null;
+    }
+
+    /**
+     * This returns a list of all headers that should be set
+     * @return array
+     */
     private function getHeaders() {
         return [
             'Authorization: Bearer '.$this->getApiKey(),
             'Autopal-Instance-ID: '.$this->getTenantId(),
-            'Access-Control-Allow-Origin: *'
+            'Access-Control-Allow-Origin: *',
+            "Accept-Encoding: gzip, deflate, sdch",
+            "Accept: */*"
         ];
     }
 
+    /**
+     * This returns a request
+     * @param $method
+     * @param $url
+     * @return resource
+     */
     private function getRequest($method, $url) {
         $request = curl_init($url);
         curl_setopt($request, CURLOPT_HEADER, 0);
@@ -74,6 +107,16 @@ class LoanPro {
         return $request;
     }
 
+    /**
+     * This performs a HTTP request and returns the response
+     * If the response is from the LoanPro API then the response will be a string holding a Json Object
+     *
+     * @param string $method The HTTP method to use (GET, POST, PUT, DELETE)
+     * @param string $uri The uri of the request (can be found at https://loanpro.simnang.com/restler/)
+     * @param array $data An associative array to be changed into a Json Object
+     * @param bool|File $file should be false if there is no file attached, otherwise it is to be the file to attach
+     * @return string The result of the request
+     */
     public function tx($method, $uri, $data = [], $file = false) {
         $url = $this->getEndpointBase().str_replace(' ', '%20', $uri);
         $method = strtoupper($method);
@@ -108,7 +151,8 @@ class LoanPro {
         rewind($verbose);
 
         $verboseLog = stream_get_contents($verbose);
-        $this->log->debug($verboseLog);
+
+        $this->logDebug($verboseLog);
 
         curl_close($request);
         return $response;
@@ -118,24 +162,61 @@ class LoanPro {
         return new ODataResourcePath($this->getEndpointBase().$property);
     }
 
+    /**
+     * Returns the uri for a URL. This is useful if you're not sure what the uri is but you do know the full URL
+     * @param $path
+     * @return mixed
+     */
     public function getUri($path) {
         $path = $path instanceof ODataResourcePath ? (string)$path : $path;
         return str_replace($this->getEndpointBase(), '', $path);
     }
 
+    /**
+     * This returns a json object holding the response of a GET request to the provided URL
+     * @param $path The full URL to send a request to
+     * @return Json
+     */
     public function odataRead($path) {
         $path = $path instanceof ODataResourcePath ? (string)$path : $path;
-        $this->log->debug($path);
+
+        $this->logDebug($path);
+
         $result = $this->tx('GET', $this->getUri($path));
-        $this->log->debug($result);
+
+        $this->logDebug($result);
         return json_decode($result);
     }
 
+    /**
+     * This returns a Json object holding the response from running an HTTP request to the uri provided
+     * @param $method The HTTP method to use
+     * @param $path The uri of the endpoint
+     * @param array $data The data to send in an associative array that will be mapped to a json object
+     * @return Json The result as a Json Objct
+     */
     public function odataRequest($method, $path, $data = []) {
         $path = $path instanceof ODataResourcePath ? (string)$path : $path;
-        $this->log->debug($path);
+
+        $this->logDebug($path);
         $result = $this->tx($method, $this->getUri($path), $data);
-        $this->log->debug($result);
+
+        $this->logDebug($result);
         return json_decode($result);
+    }
+
+    private function logDebug($info)
+    {
+        if(!is_null($this->log))
+        {
+            try{
+                $this->log->debug($info);
+            }
+            catch(\UnexpectedValueException $e)
+            {
+                echo "<script type='text/javascript'>console.log('Logging failed. Error:".str_replace("'","\\'",$e->getMessage())."'); </script>";
+                $this->log = null;
+            }
+        }
     }
 }
