b3aa3d9497177f80a5fe1e25d348e2ba2ee9810e
tofurama3000 <tolmlanmatt@gmail.com>
2015-12-21 13:28:33 -0700

    Added Phone Collections

---
+++ src/Collections/Misc/PhoneCollections.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tofurama
+ * Date: 12/21/15
+ * Time: 9:55 AM
+ */
+
+namespace Simnang\LoanPro\Collections\Loan;
+
+use Simnang\LoanPro\Collections\CollectionBase;
+
+class PaymentCollections extends CollectionBase
+{
+    private function __construct(){}
+
+    protected static $lists = [
+        "type"=>
+        [
+            "Cell"=>"cell",
+            "CellSmartPhone"=>"cellSmartPhone",
+            "Home"=>"home",
+            "Fax"=>"fax",
+            "Office"=>"office",
+            "Other"=>"other",
+        ]
+    ];
+
+    protected static $listNames = [
+        "Phone Type"=>"type"
+    ];
+}
