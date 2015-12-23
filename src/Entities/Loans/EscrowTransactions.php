8898a80439c3bc4f944f6c5f3fd79178fdddf6f5
matttolman <tolmlanmatt@gmail.com>
2015-12-23 15:32:44 -0700

    added escrow adjustments and escrow transactions

---
+++ src/Entities/Loans/EscrowTransactions.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tofurama
+ * Date: 12/21/15
+ * Time: 9:39 AM
+ */
+
+namespace Simnang\LoanPro\Entities\Loans;
+
+/**
+ * Class Collateral
+ * @package Simnang\LoanPro\Entities\Loans
+ *
+ * This represents Collateral entities used in LoanPro
+ */
+class EscrowTransactions extends \Simnang\LoanPro\Entities\BaseEntity
+{
+    /**
+     * The metadata string for it
+     * @var string
+     */
+    public $metaDataName = "EscrowTransactions";
+
+    /**
+     * Validation array for all of the collateral fields
+     * @var array
+     */
+    protected $validationArray = [
+        "numbers"=>[
+            "amount",
+        ],
+        "int"=>[
+            "id",
+            "category",
+            "subset"
+        ],
+        "date"=>[
+            "date",
+        ],
+        "collections"=>[
+            "type"=>"transaction/type",
+        ],
+        "string"=>[
+            "description"
+        ],
+    ];
+}
