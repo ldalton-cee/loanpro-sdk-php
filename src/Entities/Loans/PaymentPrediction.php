44871cb751987197b8a6fac78ddbbec9889440c9
matttolman <tolmlanmatt@gmail.com>
2015-12-23 16:00:44 -0700

    added entity for payment prediction

---
+++ src/Entities/Loans/PaymentPrediction.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tofurama
+ * Date: 12/21/15
+ * Time: 9:39 AM
+ */
+
+namespace Simnang\LoanPro\Entities\Loans;
+
+/**
+ * Class Collateral
+ * @package Simnang\LoanPro\Entities\Loans
+ *
+ * This represents Collateral entities used in LoanPro
+ */
+class PaymentPrediction extends \Simnang\LoanPro\Entities\BaseEntity
+{
+    /**
+     * Validation array for all of the collateral fields
+     * @var array
+     */
+    protected $validationArray = [
+        "numbers"=>[
+            "Amount",
+            "chargeFeeAmount",
+            "chargeFeePercentage",
+        ],
+        "int"=>[
+            "PaymentTypeId",
+        ],
+        "dates"=>[
+            "Date",
+        ],
+        "collections"=>[
+            "Extra"=>"payment/extra.tx",
+            "cardFeeType"=>"loan/cardfee.types",
+        ],
+    ];
+}
