ad585545d428d275e70e31dc03b9f83801661969
matttolman <tolmlanmatt@gmail.com>
2015-12-23 16:10:53 -0700

    added reversing payment support

--- src/Entities/Loans/Payment.php
+++ src/Entities/Loans/Payment.php
@@ -22,6 +22,19 @@ class Payment extends \Simnang\LoanPro\Entities\BaseEntity
      */
     public $metaDataName = "Payments";
 
+    public function Void($reason, $loanId, $loanProSDK)
+    {
+        $this->Reverse($reason, $loanId, $loanProSDK);
+    }
+
+    public function Reverse($reason, $loanId, $loanProSDK)
+    {
+        if(is_null($this->id))
+            return false;
+        $this->comments = $reason;
+        return $loanProSDK->odataRequest("PUT", "Loans(".$loanId.")/VoidPayment(".$this->id.")");
+    }
+
     /**
      * Validation array for all of the collateral fields
      * @var array
