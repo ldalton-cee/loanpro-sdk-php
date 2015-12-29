c582da06e3c188144bc81d678ad38f7552a6b758
tofurama3000 <tolmlanmatt@gmail.com>
2015-12-29 10:41:54 -0700

    Removed TestEnvironment from release candidate

--- TestEnvironment/composer.sh
+++
@@ -1,4 +0,0 @@
-
-cd /var/www/html/composer
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
--- TestEnvironment/setup.sh
+++
@@ -1,9 +0,0 @@
-apt-get install -y apache2
-apt-get install -y php5
-apt-get install -y git
-apt-get install -y php5-cli
-rm -rf /var/www
-ln -fs /vagrant /var/www
-apt-get update
-apt-get install -y php5-curl
-service apache2 restart
