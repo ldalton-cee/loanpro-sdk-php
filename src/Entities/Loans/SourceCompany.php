21d2394b5929756323074e2d8b05244943060770
matttolman <tolmlanmatt@gmail.com>
2016-01-25 09:33:45 -0700

    Added Source Company and Bank Account Entities

---
+++ src/Entities/Loans/SourceCompany.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tofurama
+ * Date: 12/21/15
+ * Time: 9:39 AM
+ */
+
+namespace Simnang\LoanPro\Entities\Loans;
+
+/**
+ * Class Collateral
+ * @package Simnang\LoanPro\Entities\Loans
+ *
+ * This represents Collateral entities used in LoanPro
+ */
+class SourceCompany extends \Simnang\LoanPro\Entities\BaseEntity
+{
+    /**
+     * The metadata string for it
+     * @var string
+     */
+    public $metaDataName = "SourceCompany";
+
+    /**
+     * Validation array for all of the collateral fields
+     * @var array
+     */
+    protected $validationArray = [
+        "int"=>[
+            "id",
+            "numPrefix",
+            "numSuffix"
+        ],
+        "string"=>[
+            "companyName",
+            "contactName",
+        ],
+        "class"=>[
+            "BankAccount"=>"Simnang\\LoanPro\\Entities\\Misc\\BankAccount",
+            "Address"=>"Simnang\\LoanPro\\Entities\\Customers\\Address",
+        ],
+        "email"=>[
+            "contactEmail"
+        ],
+        "phone"=>[
+            "contactPhone"
+        ],
+        "classArray"=>[
+            "CustomFieldValues"=>"Simnang\\LoanPro\\Entities\\Misc\\CustomFieldValue",
+        ]
+    ];
+}
