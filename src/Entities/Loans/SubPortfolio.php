14efce38102658a93e5ded515f375470b5cf3051
matttolman <tolmlanmatt@gmail.com>
2016-01-28 10:22:51 -0700

    Added portfolio and sub portfolio entities

---
+++ src/Entities/Loans/SubPortfolio.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tofurama
+ * Date: 12/21/15
+ * Time: 9:39 AM
+ */
+
+namespace Simnang\LoanPro\Entities\Loans;
+
+/**
+ * Class Collateral
+ * @package Simnang\LoanPro\Entities\Loans
+ *
+ * This represents Collateral entities used in LoanPro
+ */
+class SubPortfolio extends \Simnang\LoanPro\Entities\BaseEntity
+{
+    /**
+     * The metadata string for it
+     * @var string
+     */
+    public $metaDataName = "Portfolios";
+    public $entityName = 'Portfolio';
+
+    /**
+     * Validation array for all of the collateral fields
+     * @var array
+     */
+    protected $validationArray = [
+        "int"=>[
+            "id",
+            "parent"
+        ],
+        "ranges"=>[
+            "active"=>[0,1],
+        ],
+        "string"=>[
+            "title"
+        ],
+    ];
+}
